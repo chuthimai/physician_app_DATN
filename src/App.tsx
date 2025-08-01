512053466e38c490cf19e8a13624cc68f567daad
maichu <chuthimai18245@gmail.com>
2025-08-01 23:58:55 +0700

    cuộn đc phần sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import SideBar from "./components/sidebar/SideBar.tsx";
 
 function App() {
     return (
-        <div className="flex">
-            <div className="w-1/5 h-screen bg-neutral-light rounded-r-2xl shadow-2xl">
+        <div className="flex h-screen min-w-[1024px] overflow-x-auto">
+            <div className="w-1/5 h-screen bg-neutral-light rounded-r-2xl shadow-2xl flex-shrink-0">
                 <SideBar/>
             </div>
-            <div className="w-4/5 h-screen bg-neutral-light">
+            <div className="w-4/5 h-screen bg-neutral-light flex-1 overflow-auto">
                 <Outlet/>
             </div>
         </div>
--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -9,14 +9,16 @@ export default function SideBar() {
     const role = ROLES.ATTENDING_PHYSICIAN;
     const items = roleMenus[role];
     const activePath = useLocation().pathname;
-    return <div>
+    return <div className="flex flex-col h-screen">
         <HeadSideBar name={"Nguyen Van A"}/>
-        {items.map((item) => (
-            <MenuGroup
-                key={item.label}
-                item={item}
-                activePath={activePath}
-            />
-        ))}
+        <div className="flex-1 overflow-y-auto">
+            {items.map((item) => (
+                <MenuGroup
+                    key={item.label}
+                    item={item}
+                    activePath={activePath}
+                />
+            ))}
+        </div>
     </div>
 }
