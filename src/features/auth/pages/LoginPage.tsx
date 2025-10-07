cf73e4566f422aac96c29c7fe711726ca15cacc1
maichu <chuthimai18245@gmail.com>
2025-10-07 10:28:12 +0700

    sửa view LoginPage.tsx để xem luồng có chạy khi thay đổi ko

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -8,7 +8,7 @@ export default function LoginPage() {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
             <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-                <h2 className="text-2xl font-bold mb-6 text-center">Đăng nhập</h2>
+                <h2 className="text-2xl font-bold mb-6 text-center">Đăng nhập demo deploy</h2>
 
                 <LoginForm />
 
