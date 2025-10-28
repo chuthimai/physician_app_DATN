9d86fa8a948ae5a4b082e4c5d818675a81f95f37
maichu <chuthimai18245@gmail.com>
2025-10-28 17:13:39 +0700

    Thêm padding bottom cho lịch

--- src/features/work_schedules/pages/WorkSchedulesPage.tsx
+++ src/features/work_schedules/pages/WorkSchedulesPage.tsx
@@ -37,7 +37,7 @@ export default function WorkSchedulesPage() {
 
     return <div className="flex flex-col h-full">
         <h2 className="text-2xl font-bold text-center my-2 row-span-1">Lịch làm việc cá nhân</h2>
-        <div className="scroll-auto h-full w-full">
+        <div className="scroll-auto h-full w-full pb-10">
             <WorkCalendar
                 events={events}
             />
