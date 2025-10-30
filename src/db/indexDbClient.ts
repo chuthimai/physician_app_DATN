715d4a32f2a776e937de7096d62e07f1721fcd49
maichu <chuthimai18245@gmail.com>
2025-10-30 23:41:52 +0700

    Sửa lại lỗi build

--- src/db/indexDbClient.ts
+++ src/db/indexDbClient.ts
@@ -1,5 +1,5 @@
 import {openDB} from "idb";
-import type {HospitalDB} from "@/db/type/HospitalDB.ts";
+import type {HospitalDb} from "@/db/type/HospitalDb.ts";
 
 export const DB_NAME = import.meta.env.VITE_DB_NAME || "HospitalDb";
 export const DB_VERSION = import.meta.env.VITE_DB_VERSION || 1;
@@ -7,7 +7,7 @@ export const STORE_NAME = import.meta.env.VITE_STORE_NAME || "medications";
 
 // Mở kết nối IndexedDB
 export async function openMedicationDB() {
-    return openDB<HospitalDB>(DB_NAME, DB_VERSION, {
+    return openDB<HospitalDb>(DB_NAME, DB_VERSION, {
         upgrade(db) {
             if (!db.objectStoreNames.contains(STORE_NAME)) {
                 db.createObjectStore(STORE_NAME, {
